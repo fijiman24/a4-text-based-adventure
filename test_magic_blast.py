b33a0c5795478d71a4b6d22c9905600cb79c1d92
Austin He <ahe8@my.bcit.ca>
2021-03-26 12:23:11 -0700

    complete unit test

--- test_magic_blast.py
+++ test_magic_blast.py
@@ -7,31 +7,28 @@ from game import make_player
 
 
 class TestMagicBlast(TestCase):
-    def test_zero_counter(self):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_zero_counter(self, mock_output):
         player = make_player()
         player["special_action_counter"] = 0
-        actual = magic_blast(player)
+        actual_damage = magic_blast(player)
+        actual_output = mock_output.getvalue()
 
-        expected = 0
+        expected_output = "You relinquished your charges to deal \033[94m0\033[0m to the enemy!\n"
+        expected_damage = 0
 
-        self.assertEqual(actual, expected)
-
-    def test_not_zero_counter(self):
-        player = make_player()
-        player["special_action_counter"] = 3
-        actual = magic_blast(player)
-
-        expected = 15
-
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual_output, expected_output)
+        self.assertEqual(actual_damage, expected_damage)
 
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_statement(self, mock_output):
+    def test_not_zero_counter(self, mock_output):
         player = make_player()
         player["special_action_counter"] = 3
-        magic_blast(player)
-        actual = mock_output.getvalue()
+        actual_damage = magic_blast(player)
+        actual_output = mock_output.getvalue()
 
-        expected = "You relinquished your charges to deal \033[94m15\033[0m to the enemy!\n"
+        expected_output = "You relinquished your charges to deal \033[94m15\033[0m to the enemy!\n"
+        expected_damage = 15
 
-        self.assertEqual(actual, expected)
+        self.assertEqual(actual_output, expected_output)
+        self.assertEqual(actual_damage, expected_damage)
