fe42ff79633e52917d1bc21a3bf642c6905d701f
CALEB VERMA <halasmish@gmail.com>
2021-03-25 23:35:51 -0700

    Add unit tests

---
+++ test_make_enemy_boss_phase_one.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from game import make_enemy_boss_phase_one
+
+
+class Test(TestCase):
+    def test_correct_length(self):
+        boss = make_enemy_boss_phase_one()
+        expected_length = 3
+        self.assertEqual(expected_length, len(boss))
+
+    def test_correct_keys(self):
+        boss = make_enemy_boss_phase_one()
+        self.assertIn("name", boss)
+        self.assertIn("health", boss)
+        self.assertIn("maximum_damage", boss)
+
+    def test_correct_values(self):
+        boss = make_enemy_boss_phase_one()
+        self.assertEqual("Intergalactic Space Worm", boss["name"])
+        self.assertEqual(40, boss["health"])
+        self.assertEqual(5, boss["maximum_damage"])
---
+++ test_make_enemy_boss_phase_two.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from game import make_enemy_boss_phase_two
+
+
+class Test(TestCase):
+    def test_correct_length(self):
+        boss = make_enemy_boss_phase_two()
+        expected_length = 3
+        self.assertEqual(expected_length, len(boss))
+
+    def test_correct_keys(self):
+        boss = make_enemy_boss_phase_two()
+        self.assertIn("name", boss)
+        self.assertIn("health", boss)
+        self.assertIn("maximum_damage", boss)
+
+    def test_correct_values(self):
+        boss = make_enemy_boss_phase_two()
+        self.assertEqual("Two-Headed Intergalactic Space Worm", boss["name"])
+        self.assertEqual(50, boss["health"])
+        self.assertEqual(5, boss["maximum_damage"])
