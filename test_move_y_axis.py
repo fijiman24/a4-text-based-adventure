b46a38828fc4f104c423e432c2e24389b02fd5c0
Austin He <ahe8@my.bcit.ca>
2021-03-27 01:52:17 -0700

    fix formatting

--- test_move_y_axis.py
+++ test_move_y_axis.py
@@ -6,13 +6,11 @@ class TestMoveYAxis(TestCase):
     def test_move_up(self):
         actual = move_y_axis(1, 5)
         expected = 4
-
         self.assertTrue(actual, expected)
 
     def test_move_down(self):
         actual = move_y_axis(3, 6)
         expected = 7
-
         self.assertTrue(actual, expected)
 
     def test_not_valid_movement(self):
