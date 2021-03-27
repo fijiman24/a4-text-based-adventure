b3139325d5951b4d08949b60c7fd44837412491d
CALEB VERMA <halasmish@gmail.com>
2021-03-27 01:28:22 -0700

    Add unit tests

---
+++ test_game_board_coordinates.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from game import game_board_coordinates
+
+
+class Test(TestCase):
+    def test_game_board_coordinates_one_coordinate(self):
+        actual = game_board_coordinates(0, 0, 0, 0)
+        expected = {(0, 0): '\x1b[93m@\x1b[0m'}
+        self.assertEqual(expected, actual)
+
+    def test_game_board_coordinates_correct_length(self):
+        actual = len(game_board_coordinates(0, 0, 5, 5))
+        expected = 5 * 5
+        self.assertEqual(expected, actual)
+
+    def test_game_board_coordinates_unoccupied_coordinates(self):
+        actual = game_board_coordinates(0, 0, 2, 2)
+        expected = {(0, 0): '\x1b[93m@\x1b[0m', (0, 1): '*', (1, 0): '*', (1, 1): '*'}
+        self.assertEqual(expected, actual)
+
+    def test_game_board_correct_values(self):
+        board = game_board_coordinates(0, 0, 2, 2)
+        self.assertIn("*", board.values())
+        self.assertIn('\x1b[93m@\x1b[0m', board.values())
---
+++ test_turn_blue.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from game import turn_blue
+
+
+class Test(TestCase):
+    def test_turn_blue_string(self):
+        actual = turn_blue("Hello")
+        expected = "[94mHello[0m"
+        self.assertEqual(expected, actual)
+
+    def test_empty_string(self):
+        actual = turn_blue("")
+        expected = "[94m[0m"
+        self.assertEqual(expected, actual)
+
+    def test_integer_argument(self):
+        actual = turn_blue(1)
+        expected = "[94m1[0m"
+        self.assertEqual(expected, actual)
